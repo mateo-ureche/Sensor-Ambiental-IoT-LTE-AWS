0cf9abacef9fccf30a88c4b91dec521c1932f506
mateo-ureche <mateureche28@gmail.com>
2026-05-30 09:24:11 -0500

    Update Stage_03_BMP280_Test.ino

--- ESP32_Firmware/Development_Stages/Stage_03_BMP280_Test.ino
+++ ESP32_Firmware/Development_Stages/Stage_03_BMP280_Test.ino
@@ -5,19 +5,22 @@ Adafruit_BMP280 bmp;
 void setup() {
   Serial.begin(115200);
   delay(3000);
-  Wire.begin(1, 2);
+  Wire.begin(1, 42);
   if (!bmp.begin(0x76)) {
     Serial.println("BMP280 no encontrado");
-    while (1);
+    while(1);
   }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                  Adafruit_BMP280::SAMPLING_X2,
+                  Adafruit_BMP280::SAMPLING_X16,
+                  Adafruit_BMP280::FILTER_X16,
+                  Adafruit_BMP280::STANDBY_MS_500);
   Serial.println("BMP280 OK");
 }
 
 void loop() {
-  float p = bmp.readPressure() / 100.0F;
-  float t = bmp.readTemperature();
-  Serial.print("Presion: "); Serial.print(p);
-  Serial.print(" hPa  Temp: "); Serial.print(t);
-  Serial.println(" C");
   delay(2000);
+  Serial.printf("Presion: %.2f hPa  Temp: %.2f C\n",
+    bmp.readPressure() / 100.0F,
+    bmp.readTemperature());
 }
