0fac692148d2e2be2fb290170b64ad2234afc39d
mateo-ureche <mateureche28@gmail.com>
2026-05-29 22:46:32 -0500

    Update Stage_01_Serial_Test.ino

--- ESP32_Firmware/Development_Stages/Stage_01_Serial_Test.ino
+++ ESP32_Firmware/Development_Stages/Stage_01_Serial_Test.ino
@@ -1,1 +1,10 @@
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("ESP32-S3 funcionando correctamente");
+}
 
+void loop() {
+  Serial.println("Latido: " + String(millis()) + " ms");
+  delay(1000);
+}
