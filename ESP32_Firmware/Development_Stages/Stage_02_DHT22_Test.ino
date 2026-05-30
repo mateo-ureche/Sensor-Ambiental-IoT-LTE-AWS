fa2ab02cea4e2cfb6231d9cd508e7f0f729f03e1
mateo-ureche <mateureche28@gmail.com>
2026-05-29 22:48:26 -0500

    Update Stage_02_DHT22_Test.ino

--- ESP32_Firmware/Development_Stages/Stage_02_DHT22_Test.ino
+++ ESP32_Firmware/Development_Stages/Stage_02_DHT22_Test.ino
@@ -1,1 +1,24 @@
+#include <DHT.h>
+#define DHTPIN 21
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  dht.begin();
+  Serial.println("DHT22 iniciado");
+}
+
+void loop() {
+  delay(2000);
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Error: DHT22 no responde");
+    return;
+  }
+  Serial.print("Temp: "); Serial.print(t);
+  Serial.print(" C  Hum: "); Serial.print(h);
+  Serial.println(" %");
+}
